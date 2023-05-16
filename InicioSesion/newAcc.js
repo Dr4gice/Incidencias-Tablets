e8d94d5597a05c9ebfab56e229670965ea602650
Dr4gice <mariosb2004@gmail.com>
2023-05-16 09:24:52 +0200

    Login 1.4

--- InicioSesion/newAcc.js
+++ InicioSesion/newAcc.js
@@ -1,41 +1,104 @@
+const usuario = document.getElementById("username");
+const email = document.getElementById("email");
+const tipoCliente = document.getElementById("clientType");
 const contrasenya = document.getElementById("password");
 const confirmarContrasenya = document.getElementById("ConfPassword");
 const botonRegistro = document.getElementById("sendSignIn");
+const errorEmail = document.getElementById("emailError");
 const errorConfirmarContrasenya = document.getElementById("confirmPasswordError");
 const errorCamposVacios = document.getElementById("emptyFieldsError");
 
-// Comprueba si el campo Contraseña coincide con el campo Confirmar Contraseña
-contrasenya.addEventListener("input", function () {
-    if (contrasenya.value === confirmarContrasenya.value) {
-        // Habilitar el botón de registro si las contraseñas coinciden
-        botonRegistro.removeAttribute("disabled");
-        errorConfirmarContrasenya.textContent = "";
-    } else {
-        // Deshabilitar el botón de registro si las contraseñas no coinciden
-        botonRegistro.setAttribute("disabled", true);
-        errorConfirmarContrasenya.textContent = "Las contraseñas no coinciden";
-    }
-});
-
-// Comprueba si el campo Confirmar Contraseña coincide con el de Contraseña
-confirmarContrasenya.addEventListener("input", function () {
-    if (contrasenya.value === confirmarContrasenya.value) {
-        // Habilitar el botón de registro si las contraseñas coinciden
-        botonRegistro.removeAttribute("disabled");
-        errorConfirmarContrasenya.textContent = "";
-    } else {
-        // Deshabilitar el botón de registro si las contraseñas no coinciden
-        botonRegistro.setAttribute("disabled", true);
-        errorConfirmarContrasenya.textContent = "Las contraseñas no coinciden";
-    }
-});
+contrasenya.addEventListener("input", comprobarContrasenya);
+confirmarContrasenya.addEventListener("input", comprobarContrasenya);
 
 // Comprueba si los campos están vacíos. Si están vacíos muestra mensaje de error, en caso contrario se registra el usuario
-botonRegistro.addEventListener('click', function () {
-    if (document.getElementById("username").value === "" || document.getElementById("email").value === "" || document.getElementById("password").value === "" || document.getElementById("ConfPassword").value === "" || document.getElementById("clientType").value === "Elige") {
+botonRegistro.addEventListener('click', async function () {
+    if (usuario.value === "" || email.value === "" || contrasenya.value === "" || confirmarContrasenya.value === "" || tipoCliente.value === "Elige") {
         errorCamposVacios.textContent = "Completa todos los campos requeridos";
     } else {
+        if (!comprobarEmail(email.value)) {
+            email.value = "";
+            errorEmail.textContent = "Formato Email incorrecto"
+            return;
+        }
+        let contrasenyaEncriptada = await encriptar(contrasenya.value);
+        agregarUsuario(usuario.value, email.value, contrasenyaEncriptada, tipoCliente.value);
+
         location.href = '../paginaPrincipal/proyecto.html';
         errorCamposVacios.textContent = "";
     }
 });
+
+/**
+ * Comprueba si el campo Contraseña coincide con el campo Confirmar Contraseña
+ */
+function comprobarContrasenya() {
+    if (contrasenya.value === confirmarContrasenya.value) {
+        // Habilitar el botón de registro si las contraseñas coinciden
+        botonRegistro.removeAttribute("disabled");
+        errorConfirmarContrasenya.textContent = "";
+    } else {
+        // Deshabilitar el botón de registro si las contraseñas no coinciden
+        botonRegistro.setAttribute("disabled", true);
+        errorConfirmarContrasenya.textContent = "Las contraseñas no coinciden";
+    }
+}
+
+/**
+ * Añade a la lista de usuarios los datos de un nuevo usuario
+ * @param {string} nif DNI del usuario
+ * @param {string} email Email del usuario
+ * @param {string} contrasenya Contraseña del usuario
+ * @param {string} tipoCliente Tipo de usuario
+ */
+function agregarUsuario(nif, email, contrasenya, tipoCliente) {
+    let usuarioDatos = [];
+
+    try {
+        let usuarioDatos = [];
+        let listaRecuperada = JSON.parse(localStorage.getItem("listaUsuarios"));
+        usuarioDatos = listaRecuperada;
+    } catch (error) {
+        let usuarioDatos = [];
+    }
+
+    const nombreUsuario = generarNombre();
+    const usuarioJson = {
+        nif: nif,
+        email: email,
+        contrasenya: contrasenya,
+        usuario: nombreUsuario,
+        tipoCliente: tipoCliente
+    }
+
+    usuarioDatos.push(usuarioJson);
+    localStorage.setItem("listaUsuarios", JSON.stringify(usuarioDatos));
+    console.log(usuarioDatos);
+}
+
+/**
+ * Genera un nombre de usuario aleatorio
+ * @returns {string} Nombre del usuario
+ */
+function generarNombre() {
+    const nombresUsuario = ["crazy", "lazy", "happy", "sunny", "cool", "funny", "mystery", "panda", "dragon", "tiger", "lion", "eagle", "phoenix", "wolf", "fox", "hawk", "unicorn", "wizard", "ghost"];
+    const nombreAleatorio = nombresUsuario[Math.floor(Math.random() * nombresUsuario.length)];
+    const numeroAleatorio = Math.floor(Math.random() * 10000);
+    const nombreUsuario = nombreAleatorio + numeroAleatorio.toString();
+
+    return nombreUsuario;
+}
+
+/**
+ * Comprueba que el formato del email sea correcto
+ * @param {string} email Email del usuario
+ * @returns {boolean} True si es correcto, false en caso contrario
+ */
+function comprobarEmail(email) {
+    const validar = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return validar.test(email);
+}
+
+function comprobarNif(nif) {
+    return false;
+}
--- InicioSesion/signIn.js
+++ InicioSesion/signIn.js
@@ -5,24 +5,55 @@ const errorContrasenyaIncorrecta = document.getElementById("wrongPasswordError")
 
 const carpetaPaginaPrincipal = "../paginaPrincipal/";
 const principalArchivo = "proyecto.html";
-
-// Datos de ejemplo de almacenados al registrarse
-nifGuardado = "74444420K";
-contrasenyaHash = "59195c6c541c8307f1da2d1e768d6f2280c984df217ad5f4c64c3542b04111a4";
+const msgContrasenyaIncorrecta = "DNI o Contraseña incorrectos";
 
 // Comprobaciones para el Inicio de Sesión del usuario
-botonLogin.addEventListener("click", function () {
+botonLogin.addEventListener("click", async function () {
     if (nif.value === "" || contrasenya.value === "") { // Comprueba que los campos no estén vacíos
-        errorContrasenyaIncorrecta.textContent = "Completa todos los campos requeridos";
-    } else if (!(nif.value === nifGuardado)) { // Comprueba que el NIF coincida con alguno que esté guardado
+        errorContrasenyaIncorrecta.textContent = "Completa todos los campos";
+    } else if (!(await comprobarNifBD(nif.value))) { // Comprueba que el NIF coincida con alguno que esté guardado
         contrasenya.value = "";
-        errorContrasenyaIncorrecta.textContent = "El DNI o la Contraseña son incorrectos";
-    } else if (desencriptar(contrasenya.value, contrasenyaHash).then(function (esCorrecta) { // Comprueba que la Contraseña coincida con alguna que esté guardada
-        if (esCorrecta) {
+        errorContrasenyaIncorrecta.textContent = msgContrasenyaIncorrecta;
+    } else { // Comprueba que la Contraseña coincida con alguna que esté guardada
+        if (await comprobarContrasenyaBD(await encriptar(contrasenya.value))) {
             location.href = carpetaPaginaPrincipal + principalArchivo;
         } else {
             contrasenya.value = "";
-            errorContrasenyaIncorrecta.textContent = "El DNI o la Contraseña son incorrectos";
+            errorContrasenyaIncorrecta.textContent = msgContrasenyaIncorrecta;
         }
-    }));
+    }
 });
+
+function comprobarNifBD(nif) {
+    try {
+        let listaRecuperada = JSON.parse(localStorage.getItem("listaUsuarios"));
+        let encontrado = false;
+
+        listaRecuperada.forEach(usuario => {
+            if (usuario.nif === nif) {
+                encontrado = true;
+            }
+        });
+    
+        return encontrado;
+    } catch (error) {
+        return false;
+    }
+}
+
+function comprobarContrasenyaBD(contrasenya) {
+    try {
+        let listaRecuperada = JSON.parse(localStorage.getItem("listaUsuarios"));
+        let encontrado = false;
+
+        listaRecuperada.forEach(usuario => {
+            if (usuario.contrasenya === contrasenya) {
+                encontrado = true;
+            }
+        });
+    
+        return encontrado;
+    } catch (error) {
+        return false;
+    }
+}
